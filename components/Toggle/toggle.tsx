79457140c3bfd7ed8535744a6704ba119ea5cc5e
Felipe Fett <felipe.fett@hypeflame.com.br>
2021-12-17 17:12:36 -0300

    feat: improves toggle style

--- components/Toggle/toggle.tsx
+++ components/Toggle/toggle.tsx
@@ -7,7 +7,7 @@ export function Toggle() {
         <path
           id="circle"
           d="M 0,100 C 0,44.75 44.75,0 100,0 155.25,0 200,44.75 200,100 200,155.25 155.25,200 100,200 44.75,200 0,155.25 0,100"
-          style={{ strokeWidth: 5 }}
+          style={{ strokeWidth: 5, fill: "#222" }}
         />
         <text width={800} fill="white" dy={2}>
           <textPath
